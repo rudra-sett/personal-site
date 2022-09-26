801ea62cb9f86b14c6d5c25dfc19361497e63934
Rudra Sett <rudro.sett@gmail.com>
2022-09-26 10:58:57 -0400

    This is the first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        <nav className='top-strip'>
+          <p className='strip-item-right'>
+            Link 3
+          </p>
+          <p className='strip-item-right'>
+            Link 2
+          </p>
+          <p className='strip-item-right'>
+            Link 1
+          </p>
+          <p className='strip-item-left'>
+            Rudra Sett
+          </p>
+        </nav>
+        
+        {/* <h1 className="name">
+          Rudra Sett
+        </h1> */}
       </header>
+      <body className='main-content'>
+      <p className="bio">
+          Hello! This is a lot of words to show that I have words to say and mostly to fill up space until I write something actually useful.
+        </p>
+      </body>
     </div>
   );
 }
